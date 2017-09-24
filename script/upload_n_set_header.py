f6216d12fb23d7bfc79287d8f43a72c141e7e943
Alex Guo <xfguo@credosemi.com.cn>
2017-09-24 15:02:59 +0800

    upload to alioss[storage/cdn] with travis-ci.
    - use ossutil
    
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

---
+++ script/upload_n_set_header.py
@@ -0,0 +1,19 @@
+import os
+
+def print_script(cmd):
+	print("echo EXEC: ./ossutil %s " % cmd)
+	print("./ossutil -e $OSS_E -i $OSS_I -k $OSS_K %s " % cmd)
+	print("")
+
+os.chdir("_site")
+print("#!/bin/sh")
+print_script("rm -f oss://cnrv-website --recursive")
+for root, dirs, files in os.walk("./"):
+    for file in files:
+	file_name = os.path.join(root, file)
+	oss_file_name = "oss://cnrv-website/%s" % file_name[2:]
+	print_script("cp -f '_site/%s' '%s'" % (file_name, oss_file_name))
+        if file.endswith(".html"):
+            print_script("cp -f '%s' '%s'" % (oss_file_name, oss_file_name[:-5]))
+            print_script('set-meta "%s" --update "Content-Type:text/html; charset=utf-8"' % oss_file_name[:-5])
+
